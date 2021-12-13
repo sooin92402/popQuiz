5881ad720fcc2268095946f41e0613956055011c
sion <sooin92402@gmail.com>
2021-12-13 16:14:53 +0900

    first commit

---
+++ src/main/java/study/popquiz/PopQuizApplication.java
@@ -0,0 +1,13 @@
+package study.popquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/popquiz/PopQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package study.popquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
